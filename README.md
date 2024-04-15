254c5ccedb4d067c52412496c4eba34a7ccd4795
emiliano <emiliano4458@gmail.com>
2024-04-15 19:31:31 +0200

    replaced CTBot with AsyncTelegram2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # accendino_v2
-New version of accendino with telegram bot and temperature sensor
+New version of accendino with telegram bot and temperature sensor to switch heating remotely.
+
+<img src="https://i.imgur.com/HzjmjrO.jpg" style="max-width:500px;">
+
+Telegram bot:
+
+<img src="https://i.imgur.com/sAw6gSr.jpg" style="max-width:500px;">
--- accendino_v2.ino
+++ accendino_v2.ino
@@ -1,9 +1,10 @@
-#define VERSION "v1.2 13/01/2024"
+#define VERSION "Accendino_v2 v1.5 del 06/04/2024"
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "CTBot.h"
 #include "DHT.h"
+#include "secret.h"
 #define DHTPIN D3     
 #define DHTTYPE DHT11  
 #define TEMP_PIN D8
@@ -13,15 +14,16 @@
 #define OFF "off"
 #define STATUS "status"
 #define TEMP "temp"
-#define EMILIANO_ID 0000
 
 
 CTBot myBot;
 CTBotInlineKeyboard pulsante;
-String ssid = "ssid";    
-String pass = "password"; 
-String token = "token"; 
-int check_time;   
+String ssid = WIFI_SSID;    
+String pass = WIFI_PASSWORD; 
+String token = TELEGRAM_TOKEN; 
+
+int check_time;
+int msg_time;   
 
 OneWire oneWire(TEMP_PIN); 
 DallasTemperature sensors(&oneWire); 
@@ -32,9 +34,7 @@ int status=0;
 
 void setup() {
   dht.begin();
-	Serial.begin(115200);
-	Serial.println("Sto eseguendo la versione "+ String(VERSION));
-  // connect the ESP8266
+	Serial.begin(9600);
 	myBot.wifiConnect(ssid, pass);
 	myBot.setTelegramToken(token);
 
@@ -51,75 +51,106 @@ void setup() {
   pulsante.addButton("Spegni la caldaia", OFF, CTBotKeyboardButtonQuery);
   pulsante.addRow();
   pulsante.addButton("Stato accensione", STATUS, CTBotKeyboardButtonQuery);
+
+  Serial.println("Sto eseguendo la versione "+ String(VERSION));
   
   sensors.begin();
   pinMode(RELE_PIN,OUTPUT);
   check_time=millis();
-  myBot.sendMessage(EMILIANO_ID,("Ciao, sono pronto a ricevere comandi."));
+  msg_time=millis();
+  myBot.sendMessage(EMILIANO_ID,("Ciao, sono di nuovo online e pronto a ricevere comandi."));
 }
 
 
 
 void loop() {
 
-	TBMessage msg;
+	
   
-  //check connection every hour
-  if(millis()-3600000>check_time){
+  //check connection 30 minutes
+  if(millis()>check_time+1800000){
+    Serial.println("CONTROLLO LA CONNESSIONE");
     check_time=millis();  
     if (!myBot.testConnection()){
       myBot.wifiConnect(ssid, pass);
     }
   }
 
-	//if there is new message
-	if (myBot.getNewMessage(msg)) {
+  if(millis()>msg_time+30000){ //check every 30 second, otherwise consume 500 mb every 4 days
+    msg_time=millis();
+    //if there is new message
+    if (myBot.getNewMessage(msg)) {
+
+      String text=msg.text;
+      text.toLowerCase();
+      
+      if (msg.callbackQueryData.equals(HUM)) {              
+        float h = readHum();
+        myBot.sendMessage(msg.sender.id,("L'umidità è al "+String(h)+"%"));
+        sendButtons(msg); 
+      }
+
+      else if (msg.callbackQueryData.equals(TEMP)) {
+        float temp=readTemp();
+        myBot.sendMessage(msg.sender.id,("La temperatura è di "+String(temp)+"° C"));
+        sendButtons(msg); 
+      }
     
-		if (msg.callbackQueryData.equals(HUM)) {              
-      float h = readHum();
-      myBot.sendMessage(msg.sender.id,("L'umidità è al "+String(h)+"%"));
-      sendButtons(msg); 
-		}
-
-    else if (msg.callbackQueryData.equals(TEMP)) {
-      float temp=readTemp();
-      myBot.sendMessage(msg.sender.id,("La temperatura è di "+String(temp)+"° C"));
-      sendButtons(msg); 
-    }
-   
-    else if (msg.callbackQueryData.equals(ON)){
-      digitalWrite(RELE_PIN, HIGH);
-      myBot.sendMessage(msg.sender.id,("Accendo la caldaia..."));
-      status=1;
-      sendButtons(msg); 
-    }
-
-    else if (msg.callbackQueryData.equals(OFF)){
-      digitalWrite(RELE_PIN, LOW);
-      myBot.sendMessage(msg.sender.id,("Spengo la caldaia..."));
-      status=0;
-      sendButtons(msg); 
-    }
-
-    else if (msg.callbackQueryData.equals(STATUS)){
-      if(status==0){
-        myBot.sendMessage(msg.sender.id,("La caldaia è spenta"));
+      else if (msg.callbackQueryData.equals(ON)){
+        turnON();
+        myBot.sendMessage(msg.sender.id,("Ho acceso la caldaia"));
+        sendButtons(msg); 
       }
-      else{
-        myBot.sendMessage(msg.sender.id,("La caldaia è accesa"));
+
+      else if (msg.callbackQueryData.equals(OFF)){
+        turnOFF();
+        myBot.sendMessage(msg.sender.id,("Ho spento la caldaia"));
+        sendButtons(msg); 
       }
-      sendButtons(msg); 
-    }
 
-    else {                                                    
-      sendButtons(msg);  
-    }
+      else if (msg.callbackQueryData.equals(STATUS)){
+        if(status==0){
+          myBot.sendMessage(msg.sender.id,("La caldaia è spenta"));
+        }
+        else{
+          myBot.sendMessage(msg.sender.id,("La caldaia è accesa"));
+        }
+        sendButtons(msg); 
+      }
 
+      else if (text.equals("accendi")){
+        turnON();
+        myBot.sendMessage(msg.sender.id,("Ho acceso la caldaia"));
+        sendButtons(msg);
+      }
+
+      else if (text.equals("spegni")){
+        turnOFF();
+        myBot.sendMessage(msg.sender.id,("Ho spento la caldaia"));
+        sendButtons(msg); 
+      }
+
+      else {                                                    
+        sendButtons(msg);  
+      }
+
+    }
   }
 }
 
 
 //utility functions
+
+void turnON(){
+  digitalWrite(RELE_PIN, HIGH);
+  status=1;
+}
+
+void turnOFF(){
+  digitalWrite(RELE_PIN, LOW);
+  status=0;
+}
+
 float readHum(){
   return dht.readHumidity();   
 }
